525d58c0f76eecf06da15c065dfe4e5a6464ba39
alexander <s165477@student.dtu.dk>
2019-02-12 14:50:06 +0100

    init

---
+++ Galge_klient/src/galge_klient/Galge_klient.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galge_klient;
+
+/**
+ *
+ * @author alexander
+ */
+public class Galge_klient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+                
+        
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                View v = new View();
+                v.setVisible(true);
+            }
+        });
+
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ Galge_klient/src/galge_klient/Logic.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galge_klient;
+
+/**
+ *
+ * @author alexander
+ */
+public class Logic {
+    View v;
+    public Logic(){
+    }
+    
+    public void buttonPress(){
+        
+        
+    }
+    
+    
+}
